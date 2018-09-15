f1a05b40648fd5374c4a04a4288d12b322513c11
MichalAdam <mikh3x4@gmail.com>
2018-09-15 22:44:11 +0200

    moved to full object design

--- udp.py
+++ udp.py
@@ -1,7 +1,6 @@
 import socket
 import struct
 
-
 class Publisher:
     def __init__(self, typ, multicast_ip, port):
         self.struct = struct.Struct(typ)
@@ -17,30 +16,35 @@ class Publisher:
         self.sock.sendto(data, self.multicast_group)
 
     def __del__(self):
-        sock.close()
+        self.sock.close()
 
 
-def recv(ip):
-    multicast_group = ip[0]
-    server_address = ('', ip[1])
+class Subscriber:
+    def __init__(self, typ, multicast_ip, port):
+        self.struct = struct.Struct(typ)
 
-    # Create the socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Bind to the server address
-    sock.bind(server_address)
+        self.multicast_group = multicast_ip
+        self.server_address = ('', port)
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(self.server_address)
+
+        # Tell the operating system to add the socket to the multicast group
+        # on all interfaces.
+        group = socket.inet_aton(self.multicast_group)
+        mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    def recv(self):
+        data, address = self.sock.recvfrom(1024)
+        print 'received %s bytes from %s' % (len(data), address)
+        return self.struct.unpack(data)
+
+    def __del__(self):
+        self.sock.close()
 
 
-    # Tell the operating system to add the socket to the multicast group
-    # on all interfaces.
-    group = socket.inet_aton(multicast_group)
-    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
-    data, address = sock.recvfrom(1024)
-    print 'received %s bytes from %s' % (len(data), address)
-    return data
-
-ip = ('224.3.29.71', 10000)
+params = ('ff', '224.3.29.71', 10000)
 
 if __name__ == "__main__":
     msg = 'very important data'
