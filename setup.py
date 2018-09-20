f21bcdf9b2745202130dad522f89e93114ec98b2
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-09-20 03:43:28 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='UDPComms',
+      version='1.0dev',
+      description='Simple library for sending messages over UDP',
+      author='Michal Adamkiewicz',
+      author_email='mikadam@stanford.edu',
+      url='https://github.com/stanfordroboticsclub/UDP-Comms',
+      packages=['UDPComms'],
+     )
