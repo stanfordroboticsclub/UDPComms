dee40b19076093ea3be2f38f753768e047bbbf7d
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-09-21 15:19:43 +0200

    fixed gps publisher

--- gps_example.py
+++ gps_example.py
@@ -1,15 +1,12 @@
 import gps
 from UDPComms import Publisher
 
-
-## Totaly untested as of yet
-
 # GPS example code from 
 # https://learn.adafruit.com/adafruit-ultimate-gps-on-the-raspberry-pi/using-your-gps
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-fields = "time lat lon alt sats"
+fields = "time lat lon alt error_lat error_lon error_alt"
 format_ = "ifffi"
 port = 8851
 pub = Publisher(fields, format_, port)
@@ -17,31 +14,11 @@ pub = Publisher(fields, format_, port)
 while True:
     try:
     	report = session.next()
-		# Wait for a 'TPV' report and display the current time
-		# To see all report data, uncomment the line below
-		# print report
+		# Wait for a 'TPV' report
         if report['class'] == 'TPV':
-
-            time = 0
-            if hasattr(report, 'time'):
-                time = int(report.time)
-
-            latitude = 0
-            if hasattr(report, 'latitude'):
-                latitude = report.latitude
-
-            longitude = 0
-            if hasattr(report, 'longitude'):
-                longitude = report.longitude
-
-            altitude = 0
-            if hasattr(report, 'altitude'):
-                altitude = report.altitude
-
-            # if hasattr(report, 'number_of_sats'):
-            #     number_of_sats = int(report.number_of_sats)
-
-            pub.send(time,latitude,longitude,altitude,0)
+            # TODO: get time
+            pub.send(0 ,report.lat,report.lon,report.alt,
+                         report.epx, report.epy, report.epv)
 
     except KeyError:
 		pass
