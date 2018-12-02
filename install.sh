2942c4233badc3c74ffa907b4a7a740317a9c2d7
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-02 14:14:10 -0800

    install.sh now works on mac

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-#The `clean --all` removes the build directory automaticly which makes reinstalling new versions possbile with the same command.
-
-FOLDER=$(dirname $(realpath "$0"))
-
+#Uses python magic vs realpath so it also works on a mac
+FOLDER=$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")
 cd $FOLDER
+
+#The `clean --all` removes the build directory automaticly which makes reinstalling new versions possbile with the same command.
 python setup.py clean --all install
 python3 setup.py clean --all install
