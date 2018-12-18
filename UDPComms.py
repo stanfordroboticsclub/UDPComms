1491e46ca9523a2008f5b8ad2838d85d943874a1
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-18 03:22:06 -0800

    moved to using only msgpack

--- UDPComms.py
+++ UDPComms.py
@@ -12,6 +12,8 @@ import socket
 import struct
 from collections import namedtuple
 
+import msgpack
+
 from sys import version_info
 if version_info[0] < 3:
     from time import time as monotonic
@@ -20,8 +22,10 @@ else:
 
 timeout = socket.timeout
 
+MAX_SIZE = 102400
+
 class Publisher:
-    def __init__(self, fields, typ, port, local = False):
+    def __init__(self, port, local = False):
         """ Create a Publisher Object
 
         Arguments:
@@ -32,16 +36,9 @@ class Publisher:
                             work even if the computer isn't connected to a rover network. Useful for
                             development
         """
-        self.broadcast_ip = "10.0.0.255"
-
-        self.struct = struct.Struct(typ)
-        self.tuple = namedtuple("msg", fields)
-
-        assert len(self.tuple._fields) == \
-               len(self.struct.unpack( b"0"*self.struct.size ))
-
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+        self.broadcast_ip = "10.0.0.255"
         if(not local):
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             self.broadcast_ip = "127.0.0.1"
@@ -52,48 +49,31 @@ class Publisher:
         self.port = port
         self.local = local
 
-
     def send(self, *arg, **kwarg):
         """ Publish a message. The arguments are the message fields """
-        msg = self.tuple(*arg, **kwarg)
-        data = self.struct.pack(*msg)
-        self.sock.send(data)
-
-    def debug_send_type(self):
-        """ Send the message type to be verified """
-        data_to_send = self.struct.format + "," + ",".join(self.tuple._fields)
-        assert len(data_to_send) < 2048
-        self.sock.send(data_to_send)
+        msg = msgpack.dumps(*arg, **kwarg)
+        assert( len(msg) < MAX_SIZE, "Encoded message too big!" )
+        self.sock.send(msg)
 
     def __del__(self):
         self.sock.close()
 
 
 class Subscriber:
-    def __init__(self, fields, typ, port, timeout=0.2):
+    def __init__(self, port, timeout=0.2):
         """ Create a Subscriber Object
 
         Arguments:
-            fields       -- tuple of human readable names of fields in the message
-            typ          -- a struct format string that described the low level message layout
             port         -- the port to listen to messages on
             timeout      -- how long to wait before a message is considered out of date
         """
-        self.fields = fields
-        self.typ = typ
+        self.max_size = MAX_SIZE
+
         self.port = port
         self.timeout = timeout
 
-        self.struct = struct.Struct(typ)
-        self.tuple = namedtuple("msg", fields)
-        self.str_coding = str(c for c in typ if c in "sm")
-
-        self.last_message = None
+        self.last_data = None
         self.last_time = float('-inf')
-
-        assert len(self.tuple._fields) == \
-               len(self.struct.unpack( b"0"*self.struct.size ))
-
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -104,33 +84,13 @@ class Subscriber:
         self.sock.settimeout(timeout)
         self.sock.bind(("", port))
 
-    def parse_message(self, data):
-        if self.str_coding = "":
-            return self.tuple(*self.struct.unpack(data))
-        else:
-            out = []
-            i = 0
-            for elem in self.struct.unpack(data):
-                if type(elem) == bytes:
-                    if self.str_coding[i] == 's':
-                        out.append(elem.rstrip(b'\0')
-                    else:
-                        unpacker = msgpack.Unpacker()
-                        unpacker.feed(elem)
-                        out.append(unpacker.unpack())
-                    i+=1
-                else:
-                    out.append(elem)
-            return self.tuple(*out)
-
     def recv(self):
         """ Receive a message. Returns a namedtuple matching the messages fieldnames 
             If no message is received before timeout it raises a UDPComms.timeout exception"""
-        data, address = self.sock.recvfrom(self.struct.size)
-        #print('received %s bytes from %s' % (len(data), address))
-        self.last_message = self.parse_message(data)
+
+        self.last_data, address = self.sock.recvfrom(self.max_size)
         self.last_time = monotonic()
-        return self.last_message
+        return msgpack.loads(self.last_data)
 
     def get(self):
         """ Returns the latest message it can without blocking. If the latest massage is 
@@ -138,7 +98,7 @@ class Subscriber:
         try:
             self.sock.settimeout(0)
             while True:
-                self.recv()
+                self.last_data, address = self.sock.recvfrom(self.max_size)
         except socket.error:
             pass
         finally:
@@ -146,33 +106,18 @@ class Subscriber:
 
         current_time = monotonic()
         if (current_time - self.last_time) < self.timeout:
-            return self.last_message
+            return msgpack.loads(self.last_data)
         else:
             raise socket.timeout("timeout=" + str(self.timeout) + \
                                  ", last message time=" + str(self.last_time) + \
                                  ", current time=" + str(current_time))
 
-    def debug_recv_type(self):
-        """ Verify the message type matches the publisher """
-        data, address = self.sock.recvfrom(2048)
-
-        fields = data.split(",")
-        print(fields)
-        format_ = fields.pop(0)
-
-        assert format_ == self.struct.format
-        assert tuple(fields) == self.tuple._fields
-
-        return True
-
     def __del__(self):
         self.sock.close()
 
 
-params = ("left right", 'ff', 10000)
-
 if __name__ == "__main__":
     msg = 'very important data'
 
-    a = Publisher(*params)
-    a.send(ip, msg)
+    a = Publisher(1000)
+    a.send( {"text": "magic", "number":5.5, "bool":False} )
