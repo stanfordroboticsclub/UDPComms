81bf7cf1d63b105236c9bf00626550e35bdc8a54
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-18 16:34:34 -0800

    rover subcommands

--- rover.py
+++ rover.py
@@ -1,30 +1,60 @@
 #!/usr/bin/env python3
 
 import sys
-import UDPComms
+import argparse
 import json
+import time
 
-if ( len(sys.argv) != 2):
-    print("Usage: \n rover peek port_number")
-    print("TODO Usage: \n rover poke port_number rate")
-    exit()
+import UDPComms
 
-port = int(sys.argv[1])
-sub = UDPComms.Subscriber(port, timeout = 10)
 
-# peek
-while 1:
-    try:
-        print( json.dumps(sub.recv()) )
-    except UDPComms.timeout:
-        exit()
+def peek_func(port):
+    sub = UDPComms.Subscriber(port, timeout = 10)
+    while 1:
+        try:
+            data = sub.recv()
+            print("raw", data)
+            print( json.dumps(data) )
+        except UDPComms.timeout:
+            exit()
 
-# poke
-while 1:
-    line = sys.stdin.readline()
-    if line=="":
-        continue
-    print(line.rstrip('\n'))
+def poke_func(port, rate):
+    pub = UDPComms.Publisher(port, local = True)
+    data = None
+
+    while 1:
+        line = sys.stdin.readline()
+        if line!="":
+            data = line.rstrip('\n')
+
+        if data != None:
+            pub.send( json.loads(data) )
+            print( data )
+            time.sleep( rate/1000 )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='subparser')
+
+    peek = subparsers.add_parser("peek")
+    peek.add_argument('port', help="UDP port to subscribe to", type=int)
+
+    poke = subparsers.add_parser("poke")
+    poke.add_argument('port', help="UDP port to publish the data to", type=int)
+    poke.add_argument('rate', help="how often to republish (ms)", type=float)
+
+    args = parser.parse_args()
+
+    print(args)
+
+    if args.subparser == 'peek':
+        peek_func(args.port)
+    elif args.subparser == 'poke':
+        poke_func(args.port, args.rate)
+    else:
+        parser.print_help()
+
+
 
 
 
