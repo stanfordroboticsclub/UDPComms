0441501b27e5186a248b341822c3255be2b4c4cc
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-01-11 20:52:13 -0800

    install script installs rover command

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ yes | sudo pip3 install msgpack
 #The `clean --all` removes the build directory automaticly which makes reinstalling new versions possbile with the same command.
 python setup.py clean --all install
 python3 setup.py clean --all install
+
+# Install rover command
+sudo ln -s $FOLDER/rover.py /usr/local/bin/rover
