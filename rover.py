a4640e6dd90f3bc89d400a695f009058b0c6c78c
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-02-12 13:26:14 -0800

    added systemctl control to rover

--- rover.py
+++ rover.py
@@ -5,10 +5,10 @@ import argparse
 import json
 import time
 import select
+import pexpect
 
 import UDPComms
 import msgpack
-
 
 def peek_func(port):
     sub = UDPComms.Subscriber(port, timeout = 10)
@@ -43,6 +43,20 @@ def poke_func(port, rate):
             pub.send( json.loads(data) )
             time.sleep( rate/1000 )
 
+def call_func(command):
+    child = pexpect.spawn(command)
+    try:
+        child.expect('password:', timeout=20)
+    except pexpect.EOF:
+        print("Can't connect to device")
+        exit()
+    except pexpect.TIMEOUT:
+        print("Interaction with device failed")
+        exit()
+
+    child.sendline('raspberry')
+    child.interact()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='subparser')
@@ -54,16 +68,38 @@ if __name__ == '__main__':
     poke.add_argument('port', help="UDP port to publish the data to", type=int)
     poke.add_argument('rate', help="how often to republish (ms)", type=float)
 
+    commands = ['status', 'log', 'start', 'stop', 'restart', 'enable', 'disable']
+    for command in commands:
+        status = subparsers.add_parser(command)
+        status.add_argument('host', help="Which device to look for this program on")
+        status.add_argument('unit', help="The unit whose status we want to know", 
+                                                        nargs='?', default=None)
+
     args = parser.parse_args()
 
     if args.subparser == 'peek':
         peek_func(args.port)
     elif args.subparser == 'poke':
         poke_func(args.port, args.rate)
+
+    elif args.subparser in commands:
+        if args.unit is None:
+            args.unit = args.host
+
+        if args.subparser == 'status':
+            call_func("ssh pi@"+args.host+".local"+" sudo systemctl status "+args.unit)
+        elif args.subparser == 'log':
+            call_func("ssh pi@"+args.host+".local"+" sudo journalctl -f -u "+args.unit)
+        elif args.subparser == 'start':
+            call_func("ssh pi@"+args.host+".local"+" sudo systemctl start "+args.unit)
+        elif args.subparser == 'stop':
+            call_func("ssh pi@"+args.host+".local"+" sudo systemctl stop "+args.unit)
+        elif args.subparser == 'restart':
+            call_func("ssh pi@"+args.host+".local"+" sudo systemctl restart "+args.unit)
+        elif args.subparser == 'enable':
+            call_func("ssh pi@"+args.host+".local"+" sudo systemctl enable "+args.unit)
+        elif args.subparser == 'disable':
+            call_func("ssh pi@"+args.host+".local"+" sudo systemctl disable "+args.unit)
     else:
         parser.print_help()
-
-
-
-
 
