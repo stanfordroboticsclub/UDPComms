0469238396cc1e1eef5b6df177a3f0e756a07c54
Michal Adamkiewicz <mikh3x4@gmail.com>
2020-04-23 14:24:10 -0700

    made python2 explicit

--- install.sh
+++ install.sh
@@ -5,15 +5,16 @@ FOLDER=$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")
 cd $FOLDER
 
 #forces 0.6.0 until I update to new msgpack version
-yes | sudo pip install -Iv msgpack==0.6.0
+yes | sudo pip2 install -Iv msgpack==0.6.0
 yes | sudo pip3 install -Iv msgpack==0.6.0
+
+
+#The `clean --all` removes the build directory automatically which makes reinstalling new versions possible with the same command.
+python2 setup.py clean --all install
+python3 setup.py clean --all install
 
 # used for rover command
 yes | sudo pip3 install pexpect
-
-#The `clean --all` removes the build directory automaticly which makes reinstalling new versions possbile with the same command.
-python setup.py clean --all install
-python3 setup.py clean --all install
 
 # Install rover command
 sudo ln -s $FOLDER/rover.py /usr/local/bin/rover
