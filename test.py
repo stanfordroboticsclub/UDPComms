7c3bb6f3b3984d63d998b0682f75efe6f2668691
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-01-24 23:27:17 -0800

    utliproc test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 
 from UDPComms import Publisher, Subscriber, Scope, timeout
 import time
+import subprocess
 
 import unittest
 
@@ -59,7 +60,53 @@ class SingleProcessTestCase(unittest.TestCase):
         with self.assertRaises(timeout):
             self.local_sub.get()
 
+class SingleProcessNetworkTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.local_pub = Publisher(8002, scope = Scope.NETWORK )
+        self.local_sub = Subscriber(8002, timeout=1, scope = Scope.NETWORK )
+
+    def tearDown(self):
+        pass
+
+    def test_simple(self):
+        msg = [123, "testing", {"one":2} ] 
+
+        self.local_pub.send( msg )
+        recv_msg = self.local_sub.recv()
+
+        self.assertEqual(msg, recv_msg)
+
+
+
+class MultiProcessTestCase(unittest.TestCase):
+
+    def setUp(self):
+        mirror_server = """(""" \
+        """echo "from UDPComms import *";""" \
+        """echo "incomming = Subscriber(8003, timeout=5, scope = Scope.NETWORK )";""" \
+        """echo "outgoing = Publisher(8000, scope = Scope.NETWORK )";""" \
+        """echo "while 1: outgoing.send(incomming.recv())";""" \
+        """) | python"""
+
+        self.local_pub = Publisher(8003, scope = Scope.LOCAL )
+        self.return_path = Subscriber(8000, timeout = 5, scope = Scope.LOCAL )
+
+        self.p = subprocess.Popen(mirror_server, shell=True, stderr = subprocess.DEVNULL)
+        time.sleep(1)
+
+    def tearDown(self):
+        self.p.wait()
+
+    def test_simple(self):
+        msg = [123, "testing", {"one":2} ] 
+
+        self.local_pub.send( msg )
+        self.assertEqual(msg, self.return_path.recv())
+
+        msg2 = "hi"
+        self.local_pub.send(msg2)
+        self.assertEqual(msg2, self.return_path.recv())
 
 if __name__ == "__main__":
     unittest.main()
