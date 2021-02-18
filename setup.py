00690d03dc37ad746df3ae07e62a082669ad9f58
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-02-18 01:30:37 -0800

    folder?

--- setup.py
+++ setup.py
@@ -9,16 +9,17 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-exec(open(HERE / "version.py").read())
+exec(open(HERE / "UDPComms/version.py").read())
 
 setup(name='UDPComms',
       version=__version__,
-      py_modules=['UDPComms'],
       description='Simple library for sending messages over UDP',
       long_description=README,
       long_description_content_type="text/markdown",
       author='Michal Adamkiewicz',
       author_email='mikadam@stanford.edu',
+      license='MIT',
       url='https://github.com/stanfordroboticsclub/UDP-Comms',
+      packages=['UDPComms'],
       install_requires=["msgpack>=1.0.0"],
      )
